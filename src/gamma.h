19c9170fcf78e6dbf9d060f7c4c70c2c5af8f501
Konrad Skublicki <kskublicki@gmail.com>
2020-04-07 15:52:15 +0200

    gamma.c  - structures and constructor, util.h & util.c - new2dim_array

--- src/gamma.h
+++ src/gamma.h
@@ -12,6 +12,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "util.h"
+
 /**
  * Struktura przechowująca stan gry.
  */
@@ -28,8 +30,8 @@ typedef struct gamma gamma_t;
  * @return Wskaźnik na utworzoną strukturę lub NULL, gdy nie udało się
  * zaalokować pamięci lub któryś z parametrów jest niepoprawny.
  */
-gamma_t *gamma_new(uint32_t width, uint32_t height,
-                   uint32_t players, uint32_t areas);
+gamma_t *gamma_new(uint32_t width, uint32_t height, uint32_t players,
+                   uint32_t areas);
 
 /** @brief Usuwa strukturę przechowującą stan gry.
  * Usuwa z pamięci strukturę wskazywaną przez @p g.
---
+++ src/util.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+uint32_t *new_uint32_2_dimension_array(uint32_t width, uint32_t height);
